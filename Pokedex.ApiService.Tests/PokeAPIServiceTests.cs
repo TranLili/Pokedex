adcbb463ef8114c0d7976889476d8b786eadc64f
TranLili <tranlili96@gmail.com>
2025-02-01 20:53:44 +0100

    Add tests.

---
+++ Pokedex.ApiService.Tests/PokeAPIServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Pokedex.ApiService.Services;
+
+namespace Pokedex.ApiService.Tests
+{
+    [TestClass]
+    public sealed class PokeAPIServiceTests
+    {
+        private PokeAPIService _service;
+        private Mock<HttpClient> _httpClient;
+        private MemoryCache _memoryCache;
+
+        public PokeAPIServiceTests()
+        {
+            _httpClient = new Mock<HttpClient>();
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+            _service = new PokeAPIService(_httpClient.Object, _memoryCache);
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_UsingName_ReturnsPokemonData()
+        {
+            // Arrange
+            var expectedName = "bulbasaur";
+            var expectedId = 1;
+
+            // Act
+            var result = await _service.GetPokemon(expectedName);
+
+            // Assert
+            Assert.AreEqual(expectedName, result.Name.ToLower());
+            Assert.AreEqual(expectedId, result.Id);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Image));
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_UsingId_ReturnsPokemonData()
+        {
+            // Arrange
+            var expectedName = "bulbasaur";
+            var expectedId = 1;
+
+            // Act
+            var result = await _service.GetPokemon(expectedId.ToString());
+
+            // Assert
+            Assert.AreEqual(expectedName, result.Name.ToLower());
+            Assert.AreEqual(expectedId, result.Id);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Image));
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_UsingNonExistantPokemon_ReturnsNull()
+        {
+            // Arrange
+            var pokemon = "notexists";
+
+            // Act
+            var result = await _service.GetPokemon(pokemon);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_GetPokemonThenVerifyCache_CacheHit()
+        {
+            // Arrange
+            _memoryCache.Clear();
+            var pokemon = "bulbasaur";
+
+            // Act
+            var result = await _service.GetPokemon(pokemon);        
+
+            // Assert
+            var cacheResult = _memoryCache.Get(pokemon);
+            Assert.IsNotNull(cacheResult);
+            Assert.AreEqual(result, cacheResult);
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_GetPokemonByIdThenVerifyCacheByName_CacheMiss()
+        {
+            // Arrange
+            _memoryCache.Clear();
+            var pokemonName = "bulbasaur";
+            var pokemonId = 1;
+
+            // Act
+            var result = await _service.GetPokemon(pokemonId.ToString());
+
+            // Assert
+            var cacheResult = _memoryCache.Get(pokemonName);
+            Assert.IsNull(cacheResult);
+        }
+    }
+}
---
+++ Pokedex.ApiService.Tests/PokemonControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using Pokedex.ApiService.Services;
+using Pokedex.Contract.Response;
+using System.Net.Http.Json;
+using System.Net;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pokedex.ApiService.Tests
+{
+    [TestClass]
+    public class PokemonControllerTests
+    {
+        private WebApplicationFactory<Program> _factory;
+
+        private Mock<PokeAPIService> _pokeAPIServiceMock;
+        private Mock<HttpClient> _httpClientMock;
+        private Mock<IMemoryCache> _memoryCacheMock;
+        private PokeAPIService _service;
+
+        // see if response status code 200
+
+        // see if response status code 404
+
+        public PokemonControllerTests()
+        {
+            _factory = new();
+            _httpClientMock = new();
+            _memoryCacheMock = new();
+            _pokeAPIServiceMock = new(_httpClientMock.Object, _memoryCacheMock.Object);
+            _service = new(_httpClientMock.Object, new MemoryCache(new MemoryCacheOptions()));
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_ValidPokemonSearch_Returns200Ok()
+        {
+            // Arrange
+            PokemonResponse expected = new()
+            {
+                Id = 1,
+                Name = "bulbasaur",
+                Image = "bulbasaur.png"
+            };
+
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(_service);
+                });
+            }).CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/pokemon/bulbasaur");
+            var pokemon = await response.Content.ReadFromJsonAsync<PokemonResponse>();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(pokemon);
+            Assert.AreEqual(expected.Name, pokemon?.Name);
+        }
+
+        [TestMethod]
+        public async Task GetPokemon_InvalidPokemonSearch_Returns404NotFound()
+        {
+            // Arrange
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(_service);
+                });
+            }).CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/pokemon/abc");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+
+}
