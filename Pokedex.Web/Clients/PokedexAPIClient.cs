80108dc78b361205b69c15c60132b59cc0de5edf
TranLili <tranlili96@gmail.com>
2025-02-01 17:07:15 +0100

    Build frontend.

---
+++ Pokedex.Web/Clients/PokedexAPIClient.cs
@@ -0,0 +1,11 @@
+﻿using Pokedex.Contract.Response;
+
+namespace Pokedex.Web.Clients;
+
+public class PokedexAPIClient(HttpClient httpClient)
+{
+    public async Task<PokemonResponse?> GetPokemonAsync(string input, CancellationToken cancellationToken = default)
+    {
+        return await httpClient.GetFromJsonAsync<PokemonResponse>($"/pokemon/{input}", cancellationToken);
+    }
+}
--- Pokedex.Web/Program.cs
+++ Pokedex.Web/Program.cs
@@ -1,3 +1,4 @@
+using Pokedex.Contract.Response;
 using Pokedex.Web;
 using Pokedex.Web.Components;
 
@@ -12,12 +13,10 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddOutputCache();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
+builder.Services.AddHttpClient<PokemonResponse>(client =>
+{
+    client.BaseAddress = new("https+http://apiservice");
+});
 
 var app = builder.Build();
 
