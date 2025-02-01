1cec36873478573f92fc0e39cf9b7baa1f0489f6
TranLili <tranlili96@gmail.com>
2025-02-01 17:05:43 +0100

    Add response class.

---
+++ Pokedex.Contract/Response/PokemonResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Pokedex.Contract.Response;
+
+public class PokemonResponse
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string Image { get; set; }
+}
