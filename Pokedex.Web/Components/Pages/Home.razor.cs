ffe239bc06aef08d9adce7980817e1dcd0c8b1fd
TranLili <tranlili96@gmail.com>
2025-02-04 18:40:03 +0100

    Move logic from razor file to a partial class for better code separation.

---
+++ Pokedex.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Pokedex.Contract.Response;
+
+namespace Pokedex.Web.Components.Pages;
+
+public partial class Home
+{
+	private string? input;
+    private PokemonResponse? pokemon;
+    private string? errorMessage;
+
+    private async Task SearchPokemon()
+    {
+        errorMessage = null;
+        if (!string.IsNullOrEmpty(input))
+        {
+            try
+            {
+                pokemon = await pokedexAPI.GetPokemonAsync(input);
+
+                if (pokemon is null)
+                    errorMessage = "No pokémon :(";
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
+
+    private async Task EnterKeyPress(KeyboardEventArgs e)
+    {
+        if (e.Key is "Enter")
+        {
+            await SearchPokemon();
+        }
+    }
+}
